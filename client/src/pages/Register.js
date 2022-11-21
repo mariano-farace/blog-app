e265d742f1c8a439367aed2428371f7690e566ee
Mariano <mariano_farace@hotmail.com>
2022-11-21 10:33:53 +0100

    Implement Register page design

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="username"></input>
+        <input required type="email" placeholder="email"></input>
+        <input required type="password" placeholder="password"></input>
+        <button>Login</button>
+        <p>This is an error!</p>
+        <span>
+          Don you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
