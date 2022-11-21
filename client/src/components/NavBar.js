390307e0dcc32c5be0009551f73f6d8d24c39b76
Mariano <mariano_farace@hotmail.com>
2022-11-21 12:28:52 +0100

    Add alt to img

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt=""></img>
+          <img src={Logo} alt="logo"></img>
         </div>
         <div className="links">
           <Link className="link" to="/?cat=art">
