188b607e5a97b3ef1a6b82ff443ce64a83adf089
Mariano <mariano_farace@hotmail.com>
2022-11-21 09:58:13 +0100

    Setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,62 @@
 import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import SinglePost from "./pages/SinglePost";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post:id",
+        element: <SinglePost />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/register",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <RouterProvider router={router} />
     </div>
   );
 }
