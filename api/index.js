0efd8f94271c51e23ae63a9a918863f2ae9f99d1
Mariano <mariano_farace@hotmail.com>
2022-11-24 09:28:11 +0100

    setup database

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { db } from "./db.js";
+const app = express();
+
+app.use(express.json());
+
+db.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + db.threadId);
+});
+
+app.listen(8080, () => {
+  console.log("Connected on port 8080");
+});
