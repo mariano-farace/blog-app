2e49234233f48557957ec528b32e00f1fa47c5e9
Mariano <mariano_farace@hotmail.com>
2022-11-24 09:40:23 +0100

    Modify export type

--- api/db.js
+++ api/db.js
@@ -1,9 +1,11 @@
 import mysql from "mysql";
 
-export const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   port: "3306",
   user: "root",
   password: "Pendragon9*",
   database: "blog",
 });
+
+export default db;
